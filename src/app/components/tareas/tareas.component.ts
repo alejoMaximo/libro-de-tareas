f16eac30204338016aaa2422b04a4287f5f5d3d7
alejoMaximo <alejomaximo1990@gmail.com>
2022-12-21 22:44:50 -0500

    mi lista

---
+++ src/app/components/tareas/tareas.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { tarea } from 'src/app/models/tarea';
+
+@Component({
+  selector: 'app-tareas',
+  templateUrl: './tareas.component.html',
+  styleUrls: ['./tareas.component.css']
+})
+export class TareasComponent implements OnInit {
+  
+  listaTareas: tarea[]=[];
+  nombreTarea:string='';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  Agregartarea(){
+
+    const tarea: tarea ={
+       nombre: this.nombreTarea,
+       estado:false
+    }
+
+    this.listaTareas.push(tarea);
+
+    this.nombreTarea='';
+  }
+
+  eliminar(i:number){
+    this.listaTareas.splice(i,1);
+
+  }
+
+  actualizartarea(tarea:tarea, i:number)
+  {
+    this.listaTareas[i].estado = !tarea.estado;
+  }
+
+}
